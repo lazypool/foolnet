e3e7e2ef1c38eeae12aa9d4bc5bc25516e6782bb
lazypool <3416382833@qq.com>
2024-03-08 15:03:34 +0800

    v0.3.0 - 重构中间层类逻辑

--- foolnet/network/layers/__init__.py
+++ foolnet/network/layers/__init__.py
@@ -1,7 +1,28 @@
 import numpy as np
 
+from .. import Component
 
-class Layer:
+
+class Layer(Component):
+    """
+    Base class for middle layer.
+
+    Attribute:
+        output (np.ndarray):
+            Output of this layer, updated by `forward`.
+        dinputs (np.ndarray):
+            Derivative of loss to this layer's inputs, updated by `backward`.
+
+    Methods:
+        forward(inputs: np.ndarray) -> None:
+            - `inputs` is the output of last layer (activation layer OR pooling layer)
+            - `inputs` shape [B, H]
+        backward(delta: np.ndarray) -> None:
+            - `delta` is derivative of loss to this layer's output
+        update(lr: float) -> None:
+            update the parameters of this layer. `lr` means 'learning rate'.
+    """
+
     def __init__(self) -> None:
         self.output = np.ndarray(0)
         self.dinputs = np.ndarray(0)
@@ -12,4 +33,8 @@ class Layer:
 
     def backward(self, delta: np.ndarray) -> None:
         if delta != None:
+            raise NotImplementedError
+
+    def update(self, lr: float) -> None:
+        if lr != None:
             raise NotImplementedError
--- foolnet/network/layers/denseLayer.py
+++ foolnet/network/layers/denseLayer.py
@@ -1,25 +1,46 @@
 import numpy as np
 
-from ..activation import Activation
 from . import Layer
 
 
 class DenseLayer(Layer):
-    def __init__(self, n_inputs: int, n_output: int, activation: Activation) -> None:
+    """
+    DenseLayer class, used in fully connected net.
+
+    Args:
+        n_inputs (int):
+            The dimension of the inputed tensors.
+        n_output (int):
+            The dimension of the outputed tensors.
+
+    Attribute:
+        weight (np.ndarray):
+            Weight to multiply with inputs, shape [n_inputs, n_output].
+        bias (np.ndarray):
+            Bias to add with the weighted sum, shape [1, n_output].
+        d_weight (np.ndarray):
+            The derivative of loss to this layer's weight, share same shape with `weight`.
+        d_bias (np.ndarray):
+            The derivative of loss to this layer's bias, share same shape with `bias`.
+    """
+
+    def __init__(self, n_inputs: int, n_output: int) -> None:
+        self.classname = f"DenseLayer([{n_inputs}, {n_output}])"
+
+        # initialize parameters
         self.weight = 0.01 * np.random.randn(n_inputs, n_output)
         self.bias = np.zeros((1, n_output))
-        self.activation = activation
+        self.d_weight = np.ndarray(0)
+        self.d_bias = np.ndarray(0)
 
     def forward(self, inputs: np.ndarray) -> None:
         self.inputs = np.array(inputs)
-        self.logits = np.dot(self.inputs, self.weight) + self.bias
-        self.output = self.activation(self.logits)
+        self.output = np.dot(self.inputs, self.weight) + self.bias
 
     def backward(self, delta: np.ndarray) -> None:
-        self.delta = self.activation(delta, derivative=True)
-        self.d_weight = np.dot(self.inputs.T, self.delta)
-        self.d_bias = np.sum(self.delta, axis=0, keepdims=True)
-        self.dinputs = np.dot(self.delta, self.weight.T)
+        self.d_weight = np.dot(self.inputs.T, delta)
+        self.d_bias = np.sum(delta, axis=0, keepdims=True)
+        self.dinputs = np.dot(delta, self.weight.T)
 
     def update(self, lr: float) -> None:
         self.weight += -lr * self.d_weight
